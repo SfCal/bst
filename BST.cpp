ddfbb54b1c364042b7e26f57e452d6935451db07
Sam <samuel.f.calvert@gmail.com>
2020-10-27 17:22:04 -0400

    insert function working

--- BST.cpp
+++ BST.cpp
@@ -11,162 +11,169 @@ using namespace std;
 
 
 BST::BST() {
-	root = NULL;
+    root = NULL;
 }
 BST::BST(string s) {
-	root = new TNode(s);
+    root = new TNode(s);
 }
 
 void BST::printTreeIO() {
-	if (root == NULL ) {
-		cout << "Empty Tree" << endl;
-	}
-	else {
-		cout << endl<<"Printing In Order:" <<endl;
-		printTreeIO(root);
-	}
+    if (root == NULL ) {
+        cout << "Empty Tree" << endl;
+    }
+    else {
+        cout << endl<<"Printing In Order:" <<endl;
+        printTreeIO(root);
+    }
 }
 
 
 void BST::printTreePre() {
-	if (root == NULL ) {
-		cout << "Empty Tree" << endl;
-	}
-	else {
-		cout << endl<<"Printing PreOrder:" <<endl;
-		printTreePre(root);
-	}
+    if (root == NULL ) {
+        cout << "Empty Tree" << endl;
+    }
+    else {
+        cout << endl<<"Printing PreOrder:" <<endl;
+        printTreePre(root);
+    }
 }
 
 
 void BST::printTreePost() {
-	if (root == NULL ) {
-		cout << "Empty Tree" << endl;
-	}
-	else {
-		cout << endl<<"Printing PostOrder:" <<endl;
-		printTreePost(root);
-	}
+    if (root == NULL ) {
+        cout << "Empty Tree" << endl;
+    }
+    else {
+        cout << endl<<"Printing PostOrder:" <<endl;
+        printTreePost(root);
+    }
 }
 
 
 void BST::clearTree() {
-	if (root == NULL) {
-		cout << "Tree already empty" << endl;
-	}
-	else {
-		cout << endl << "Clearing Tree:" << endl;
-		clearTree(root);
-		root = NULL;
-	}
+    if (root == NULL) {
+        cout << "Tree already empty" << endl;
+    }
+    else {
+        cout << endl << "Clearing Tree:" << endl;
+        clearTree(root);
+        root = NULL;
+    }
 }
 void BST::clearTree(TNode *tmp) {
-	if (tmp == NULL) {
-		return;
-	}
-	else {
-		clearTree(tmp->left);
-		clearTree(tmp->right);
-		tmp->printNode();
-		delete(tmp);
-	}
+    if (tmp == NULL) {
+        return;
+    }
+    else {
+        clearTree(tmp->left);
+        clearTree(tmp->right);
+        tmp->printNode();
+        delete(tmp);
+    }
 }
 
 bool BST::insert(string s){
-	TNode *temp;
-	if (root==NULL){
-		root = new TNode(s);
-	}
-	else{
-		temp = root;
-		while (temp!=NULL){
-			if (s<temp->data->phrase){
-				if (temp->left==NULL){
-					temp->left = new TNode(s);
-					setHeight(temp->left);
-					temp->left->parent = temp;
-					return true;
-				}
-				else{
-					temp = temp->left;
-				}
-			}
-			else if (s>temp->data->phrase){
-				if (temp->right==NULL){
-					temp->right = new TNode(s);
-					setHeight(temp->right);
-					temp->right->parent = temp;
-					return true;
-				}
-				else{
-					temp = temp->right;
-				}
-			}
-			else{
-				return false;
-			}
-		}
-	}
+    TNode *temp;
+    if (root==NULL){
+        root = new TNode(s);
+    }
+    else{
+        temp = root;
+        while (temp!=NULL){
+            if (s<temp->data->phrase){
+                if (temp->left==NULL){
+                    temp->left = new TNode(s);
+                    temp->left->parent = temp;
+                    setHeight(temp->left);
+                    return true;
+                }
+                else{
+                    temp = temp->left;
+                }
+            }
+            else if (s>temp->data->phrase){
+                if (temp->right==NULL){
+                    temp->right = new TNode(s);
+                    temp->right->parent = temp;
+                    setHeight(temp->right);
+                    return true;
+                }
+                else{
+                    temp = temp->right;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+    }
 }
 
 int BST::maxOfHeights(TNode *m, TNode *n){
-	if (m->height > n->height){
-		return m->height;
-	}
-	else{
-		return n->height;
-	}
+    if (m==NULL){
+        return n->height;
+    }
+    if (n==NULL){
+        return m->height;
+    }
+    if (m->height > n->height){
+        return m->height;
+    }
+    else{
+        return n->height;
+    }
 }
 
 void BST::setHeight(TNode *n){
-	if (n->height==1){
-		if (n->parent->height==maxOfHeights(n->left,n->right)){
-			return;
-		}
-	}
-	else{
-		n->parent->height += 1;
-		setHeight(n->parent);
-	}
+    if (n->parent!=NULL){
+        if (n->parent->height==maxOfHeights(n->parent->left,n->parent->right)+1||n->parent==NULL){
+            return;
+        }
+        else{
+            //cout << "else part"<<endl;
+            n->parent->height += 1;
+            setHeight(n->parent);
+        }
+    }
 }
 
 TNode* BST::find(string s){
 }
 
 void BST::printTreeIO(TNode *n){
-	if (n==NULL){
-		return;
-	}
-	else{
-		printTreeIO(n->left);
-		cout << n->data->phrase<<","<<n->height;
-		cout << endl;
-		printTreeIO(n->right);
-	}
+    if (n==NULL){
+        return;
+    }
+    else{
+        printTreeIO(n->left);
+        cout << n->data->phrase<<","<<n->height;
+        cout << endl;
+        printTreeIO(n->right);
+    }
 }
 
 void BST::printTreePre(TNode *n){
-	if (n==NULL){
-		return;
-	}
-	else{
-		cout << n->data->phrase<<","<<n->height;
-		cout << endl;
-		printTreePre(n->left);
-		printTreePre(n->right);
-	}
+    if (n==NULL){
+        return;
+    }
+    else{
+        cout << n->data->phrase<<","<<n->height;
+        cout << endl;
+        printTreePre(n->left);
+        printTreePre(n->right);
+    }
 }
 
 void BST::printTreePost(TNode *n){
-	if (n==NULL){
-		return;
-	}
-	else{
-		printTreePost(n->left);
-		printTreePost(n->right);
-		cout << n->data->phrase<<","<<n->height;
-		cout << endl;
-	}
+    if (n==NULL){
+        return;
+    }
+    else{
+        printTreePost(n->left);
+        printTreePost(n->right);
+        cout << n->data->phrase<<","<<n->height;
+        cout << endl;
+    }
 }
 
 TNode* BST::remove(string s){
